5acf85b153b7bb32d9048960bbbe3b9dafa6516e
Kristian Rother <kristian@spiced-academy.com>
2020-11-29 11:09:49 +0100

    add roadmap, but not sure yet what it is good for

---
+++ Python_Roadmap/python_first_steps.py
@@ -0,0 +1,64 @@
+## 1 First Steps
+
+* Data types: int(), float(), str(), list()
+* Builtins: print(), input(), range(), len()
+* keywords: if, for
+* operators: +-*/ =>< and or
+
+###  1.1 Installation
+
+* install Anaconda
+* start Spyder
+* create a `.py` file
+* use keyboard shortcuts
+* know where the official Python documentation is
+
+### 1.2 Hello World
+
+* read data from the keyboard
+* store a string in a variable
+* print the contents of a variable
+* comment your code
+
+### 1.3 Python as a calculator
+
+* use the arithmetical operators (`+ - * / ** // %`)
+* distinguish integer and float numbers
+* convert integers, floats and strings
+* use functions from the `math` module
+* look up functions from the `math` module
+* calculate absolute and rounded values
+
+### 1.4 Lists
+
+* store data in a list
+* create data with `range()`
+* examine the length of a list
+* sort and slice lists
+* draw a bar plot
+
+### 1.5 Loops
+
+* iterate over lists and ranges using a for loop
+* indent a code block
+* distinguish iterables and loop variables
+* count the current iteration using a variable
+* collect results of a calculation in a list
+
+### 1.6 Conditions
+
+* write `if..elif..else` statements
+* count items in a list by a condition
+* filter a list by a condition
+* combine comparison operators with boolean logic
+* modify every second entry of a list
+* enumerate operators you can use in comparisons
+
+### 1.7 Debugging
+
+* code, run, debug and repeat
+* recognize and fix SyntaxErrors
+* recognize and fix IndentationErrors
+* recognize and fix runtime Exceptions
+* execute code partially and inspect the result
+* look up the documentation of functions
---
+++ Python_Roadmap/python_roadmap.md
@@ -0,0 +1,27 @@
+
+# Roadmap for Python Beginners
+
+* Linux command line
+* git
+
+## Path of the Web Developer
+
+* Bootstrap
+* Djangogirls tutorial
+
+bottle, requests, regex, bs4
+
+## Path of the Data Analyst
+
+* Jupyter notebook
+* matplotlib
+* pandas
+* scikit-learn
+
+## Path of the Scientific software developer
+
+* virtualenv
+* algorithms
+* classes
+* automated testing
+
